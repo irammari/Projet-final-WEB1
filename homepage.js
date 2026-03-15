3f77b1014969322b192d24d048965e35b974ab05
Iram <hei.iram.67@gmail.com>
2026-03-15 08:40:06 +0300

    Add cart panel and style it

--- homepage.js
+++ homepage.js
@@ -1,2 +1,15 @@
 document.querySelector(".about1").textContent = data.aboutMe_part1;
 document.querySelector(".about2").textContent = data.aboutMe_part2;
+
+const container = document.querySelector(".about-me-xp");
+
+data.overview.forEach(item => {
+  const block = document.createElement("div");
+
+  block.innerHTML = `
+    <div class="xp-num">${item.number}</div>
+    <div class="xp-text">${item.label}</div>
+  `;
+
+  container.appendChild(block);
+});
