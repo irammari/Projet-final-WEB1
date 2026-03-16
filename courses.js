efed063964844d59015cad1b3dcde74e73fcb48d
Iram <hei.iram.67@gmail.com>
2026-03-16 13:22:04 +0300

    add gallery grid for courses add and make filters for courses functional

--- courses.js
+++ courses.js
@@ -2,6 +2,7 @@ const priceMin = document.getElementById("price-min");
 const priceMax = document.getElementById("price-max");
 const priceMinSlider = document.getElementById("price-min-slider");
 const priceMaxSlider = document.getElementById("price-max-slider");
+const clearFiltersBtn = document.getElementById("clearFiltersBtn");
 
 function setPrice() {
     priceMin.textContent = Number(priceMinSlider.value).toLocaleString("en-US");
@@ -38,12 +39,12 @@ function createCourseCard(course) {
 
             <div class="course-card-info">
                 <h3 class="course-title">${course.title}</h3>
-                <p class="course-price">${course.price.toLocaleString("en-US")}</p>
+                <p class="course-price">${course.price.toLocaleString("en-US")} Ar</p>
                 <p class="course-description">${course.description}</p>
             </div>
 
             <div class="course-card-btns">
-                <button class="course-learn-more-btn">Learn about me</button>
+                <button class="course-learn-more-btn">Learn more</button>
                 <button class="course-book-btn">Add to cart</button>            
             </div>
         </div>
@@ -52,6 +53,77 @@ function createCourseCard(course) {
 
 const gallery = document.querySelector(".courses-gallery");
 
-data.courses.forEach(course => {
-    gallery.innerHTML += createCourseCard(course);
+function showCourses(coursesArray) {
+    gallery.innerHTML = "";
+    coursesArray.forEach(course => {
+        gallery.innerHTML += createCourseCard(course);
+    });
+}
+showCourses(data.courses);
+
+const langIcons = document.querySelectorAll(".lang-icons span");
+const techFilter = document.getElementById("tech-select");
+const levelFilter = document.getElementById("level-select");
+const priceMinFilter = document.getElementById("price-min-slider");
+const priceMaxFilter = document.getElementById("price-max-slider");
+const searchFilter = document.querySelector(".search-filter input");
+
+let selectedLangs = [...document.querySelectorAll(".lang-icons span.active")].map(icon => icon.id);
+langIcons.forEach(icon => icon.classList.add('active'));
+
+langIcons.forEach(icon => {
+    icon.addEventListener('click', () => {
+        icon.classList.toggle('active');
+        selectedLangs = [...document.querySelectorAll(".lang-icons span.active")].map(icon => icon.id);
+        if (selectedLangs.length === 0) {
+            selectedLangs = [...document.querySelectorAll(".lang-icons span")].map(icon => icon.id);
+        }
+
+        filter();
+    });
 });
+
+techFilter.addEventListener('change', filter);
+levelFilter.addEventListener('change', filter);
+priceMinFilter.addEventListener('input', filter);
+priceMaxFilter.addEventListener('input', filter);
+searchFilter.addEventListener('input', filter);
+
+function filter() {
+    const selectedTech = techFilter.value;
+    const selectedLevel = levelFilter.value;
+    const selectedMinPrice = Number(priceMinFilter.value);
+    const selectedMaxPrice = Number(priceMaxFilter.value);
+    const searchedKeywords = searchFilter.value.toLowerCase();
+
+    const filtered = data.courses.filter(course => {
+
+        const langMatch = selectedLangs.length === 0 || selectedLangs.includes(course.language);
+        let techMatch = selectedTech === "all" || 
+            (course.technologies && course.technologies.includes(selectedTech));
+
+        let levelMatch = selectedLevel === "all" ||
+            course.level.toLowerCase() === selectedLevel.toLowerCase();      
+
+        let minPriceMatch = course.price >= selectedMinPrice;
+        let maxPriceMatch = course.price <= selectedMaxPrice;
+
+        let searchedMatch = course.title.toLowerCase().includes(searchedKeywords) ||
+            course.description.toLowerCase().includes(searchedKeywords); 
+
+        return langMatch && techMatch && levelMatch && minPriceMatch && maxPriceMatch && searchedMatch;
+    })
+    filtered.sort((a, b) => a.price - b.price);
+    showCourses(filtered);
+}
+
+clearFiltersBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    langIcons.forEach(icon => icon.classList.add('active'));
+    techFilter.value = "all";
+    levelFilter.value = "all";
+    priceMinFilter.value = "0"
+    priceMaxFilter.value = max;
+    searchFilter.value = null;
+    showCourses(data.courses);
+})
